409cffc695f51cd3ad0613e8bcb94517af92547a
Tanathorn-Rin <so.tanathorn@gmail.com>
2025-10-24 03:25:21 +0700

    Adding state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../component/movie";
+import React from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = React.useState("");
+
     const movies = [
         { id: 1, title: "Inception", releaseDate: "2010" },
         { id: 2, title: "The Matrix", releaseDate: "1999" },
@@ -9,18 +12,24 @@ function Home() {
 
     const handleSearchSubmit = (event) => {
         event.preventDefault();
-        const query = event.target.elements.searchInput.value;
-        console.log("Searching for:", query);
+        alert("Searching for " + searchQuery);
+        setSearchQuery("------------");
     }
 
     return (
         <div className="home">
             <form onSubmit={handleSearchSubmit} className="search-form">
-                <input type="text" name="searchInput" placeholder="Search for a movie..." className="search-input" />
+                <input type="text" name="searchInput" 
+                placeholder="Search for a movie..." 
+                className="search-input" 
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movie-grid">
-                {movies.map(movie => (<MovieCard movie={movie} key={movie.id}/>))}
+                {movies.map(movie => 
+                    movie.title.toLocaleLowerCase().startsWith(searchQuery) && (<MovieCard movie={movie} key={movie.id}/>))}
             </div>
         </div>
     );
